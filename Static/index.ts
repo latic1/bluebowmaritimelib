04d684846da2dd9c3bb7fe0964f83f4a25fbf247
latic1 <laticlatif2@gmail.com>
2024-11-01 09:58:55 +0000

    first commit

--- Static/index.ts
+++ Static/index.ts
@@ -2,18 +2,18 @@ export const slides = [
   {
     image: "/img/slied1.jpg",
     header: "Your Partner in Marine Services Excellence",
-    details: "Transforming Ideas into Powerful Software Solutions",
+    details: "leoremt is a long established fact that a reader will be distracted by the readable content of",
   },
   {
     image: "/img/slied3.jpg",
     header: "Your Partner in Marine Services Excellence",
     details:
-      "Turning Visions into Seamless Online Experiences. Your Website, Your Story.",
+      " is a long established fact that a reader will be distracted by the readable content of",
   },
   {
     image: "/img/slied2.jpg",
     header:
       "Your Partner in Marine Services Excellence",
-    details: "Your Pathway to Efficency and Innovations",
+    details: "Your t is a long established fact that a reader will be distracted by the readable content of",
   },
 ];
